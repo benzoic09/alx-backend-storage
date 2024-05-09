b210c6fe8f421a09fcf7c46f5804b4e416ec71f2
Ben Kimani <benzoic09@gmail.com>
2024-05-09 18:29:30 +0000

    task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,7 @@
 -- SQL script that ranks country origins of bands, 
 -- ordered by the number of (non-unique) fans
+
 SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
-GROUP BY orgin
+GROUP BY origin
 ORDER BY nb_fans DESC;
