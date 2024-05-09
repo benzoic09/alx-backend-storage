3a90d19a5fa94fff5c24d9e030c30e3d9739ab98
Ben Kimani <benzoic09@gmail.com>
2024-05-09 18:17:17 +0000

    task 2

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,4 @@
--- cript that creates a table users
+-- script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
 	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
