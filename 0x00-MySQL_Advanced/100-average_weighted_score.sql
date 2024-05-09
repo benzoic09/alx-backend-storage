8cc0f077b605ef1863be52ed1b56b5945f666361
Ben Kimani <benzoic09@gmail.com>
2024-05-09 21:39:19 +0000

    task 100

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -5,15 +5,13 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
 	-- Compute the average weighted score for the user
-    UPDATE users SET average_score = (SELECT
+	UPDATE users SET average_score = (SELECT
 	SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
 	FROM corrections
 	INNER JOIN projects
 	ON projects.id = corrections.project_id
 	WHERE corrections.user_id = user_id)
 	WHERE users.id = user_id;
-    
 END $$
-
 DELIMITER ;
 
