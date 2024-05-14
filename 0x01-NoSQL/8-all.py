0760729b5da55d02f899274140ab2845105790ec
Ben Kimani <benzoic09@gmail.com>
2024-05-14 13:59:08 +0000

    task 9

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -7,7 +7,7 @@ from typing import List, Dict
 def list_all(mongo_collection):
     """function that lists all documents in a collection:"""
     documents = mongo_collection.find()
-    if documents.count() == 0;
-    return []
+    if documents.count() == 0:
+        return []
     
     return documents
