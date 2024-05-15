63f2ee6bb7a814cdfa1718465c0b390d50307565
Ben Kimani <benzoic09@gmail.com>
2024-05-15 19:56:29 +0000

    cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -24,3 +24,48 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float, None]:
+        """Retrieve data from Redis and optionally convert it using a
+        provided function.
+
+        Args:
+            key (str): The key under which the data is stored.
+            fn (Optional[Callable]): A callable to convert
+            the data back to the desired format.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data,
+            optionally converted.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """Retrieve a string value from Redis.
+
+        Args:
+            key (str): The key under which the data is stored.
+
+        Returns:
+            Union[str, None]: The retrieved string value or
+            None if the key does not exist.
+        """
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """Retrieve an integer value from Redis.
+
+        Args:
+            key (str): The key under which the data is stored.
+
+        Returns:
+            Union[int, None]: The retrieved integer value or
+            None if the key does not exist.
+        """
+        return self.get(key, lambda x: int(x))
