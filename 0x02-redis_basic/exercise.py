0912937fb92200e28855f4e762943aac7c8ca4dd
Ben Kimani <benzoic09@gmail.com>
2024-05-15 20:43:27 +0000

    cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,6 +19,7 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
 class Cache:
     def __init__(self):
         """Initialize the Cache with a Redis client and flush the database."""
@@ -39,15 +40,19 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
-        """Retrieve data from Redis and optionally convert it using a provided function.
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float, None]:
+        """Retrieve data from Redis and optionally
+        convert it using a provided function.
 
         Args:
             key (str): The key under which the data is stored.
-            fn (Optional[Callable]): A callable to convert the data back to the desired format.
+            fn (Optional[Callable]): A callable to convert the
+            data back to the desired format.
 
         Returns:
-            Union[str, bytes, int, float, None]: The retrieved data, optionally converted.
+            Union[str, bytes, int, float, None]:
+            The retrieved data, optionally converted.
         """
         value = self._redis.get(key)
         if value is None:
@@ -63,7 +68,8 @@ class Cache:
             key (str): The key under which the data is stored.
 
         Returns:
-            Union[str, None]: The retrieved string value or None if the key does not exist.
+            Union[str, None]: The retrieved string value or
+            None if the key does not exist.
         """
         return self.get(key, lambda x: x.decode('utf-8'))
 
@@ -74,6 +80,7 @@ class Cache:
             key (str): The key under which the data is stored.
 
         Returns:
-            Union[int, None]: The retrieved integer value or None if the key does not exist.
+            Union[int, None]: The retrieved integer value or
+            None if the key does not exist.
         """
         return self.get(key, lambda x: int(x))
