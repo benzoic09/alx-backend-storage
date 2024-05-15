1427e7e2720a6848eb5913a9815a2f98618e9291
Ben Kimani <benzoic09@gmail.com>
2024-05-15 19:38:16 +0000

    cache class

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Cache class that stores data in Redis."""
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        """Initialize the Cache with a Redis client and flush the database."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store the data in Redis with a randomly generated key.
+
+        Args:
+            data (Union[str, bytes, int, float]): The data to be stored.
+
+        Returns:
+            str: The key under which the data is stored.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
